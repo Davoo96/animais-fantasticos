8d62d02248ba4acdb32b233a6d4f2b91fbd15d3f
Davoo96 <davizi06@hotmail.com>
2022-01-04 22:05:17 -0300

    Project Animais Fantasticos - Origamid

---
+++ script.js
@@ -0,0 +1,25 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiroUl = document.querySelector('ul');
+console.log(primeiroUl);
+
+const linkInternos = document.querySelector('[href^="#"]');
+console.log(linkInternos);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg);
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+
+
+console.log(gridSectionHTML);
+console.log(gridSectionNode);
