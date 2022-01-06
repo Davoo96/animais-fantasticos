32657f8a8241452eaa94a4697159a6165c15386f
Davoo96 <davizi06@hotmail.com>
2022-01-06 14:40:38 -0300

    More animations

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import animacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+initScrollSuave();
+animacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
